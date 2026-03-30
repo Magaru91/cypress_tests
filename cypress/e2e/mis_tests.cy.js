752fe1744da9d558dd607dcd04bde05c62f243cb
maria garcia <mariaypamella@gmail.com>
2026-03-30 14:53:17 +0200

    move tests to e2e

--- cypress/e2e/mis_tests.cy.js
+++ cypress/e2e/mis_tests.cy.js
@@ -29,14 +29,33 @@ describe('Tests de tareas', () => {
     cy.get('.todo-list li').should('not.have.class', 'completed')
   })
 
-})it('elimina una tarea', () => {
+  it('elimina una tarea', () => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+    cy.get('.new-todo').type('Tarea 1{enter}')
+
+    cy.get('.todo-list li')
+      .trigger('mouseover')
+      .find('.destroy')
+      .click({ force: true })
+
+    cy.get('.todo-list li').should('not.exist')
+  })
+it('filtra tareas completadas',() => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/')
 
-  cy.get('.new-todo').type('Tarea 1{enter}')
+  cy.get('.new-todo').type('tarea 1{enter}')
+  cy.get('.new-todo').type('tarea2 {enter}')
 
-  cy.get('.todo-list li').trigger('mouseover') // para que aparezca la X
+  cy.get('.toggle').first().click()
+ 
+cy.contains('Completed').click()
 
-  cy.get('.destroy').click()
+cy.get('.todo-list li').should('have.length', 1)
 
-  cy.get('.todo-list li').should('not.exist')
+cy.get('.todo-list li').should('have.class','completed')
 })
+
+
+})
+
